e0f374ef4a2a5515c09fec76ed43fe19e7da1652
pnpk <pnpk00@gmail.com>
2020-10-01 02:35:13 +0900

    commit

---
+++ Makefile
@@ -0,0 +1,2 @@
+test:
+	./vender/bin/phpunit
---
+++ src/JapaneseCalender.php
@@ -0,0 +1,12 @@
+<?php
+namespace JapaneseCalender;
+
+use DateTime;
+
+class JapaneseCalender
+{
+    public function getEra(DateTime $date)
+    {
+        //
+    }
+}
