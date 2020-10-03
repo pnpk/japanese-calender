32bf79f769615280a4b4ea202fc7669699b3dbd5
pnpk <pnpk00@gmail.com>
2020-10-04 08:48:47 +0900

    テスト作成

--- tests/JapaneseCalenderTest.php
+++ tests/JapaneseCalenderTest.php
@@ -1,26 +1,94 @@
 <?php
 namespace tests;
 
+require "./vendor/larapack/dd/src/helper.php";
+
 use PHPUnit\Framework\TestCase;
 use pnpk\JapaneseCalender\JapaneseCalender;
-
-use function PHPUnit\Framework\assertFalse;
+use Carbon\Exceptions\InvalidFormatException;
+use function PHPUnit\Framework\assertEquals;
 
 class JapaneseCalenderTest extends TestCase
 {
     /**
      * @test
      */
-    public function hoge()
+    public function 令和の年号が取得できる()
     {
-        $dateTimeZoneJapan = new \DateTimeZone("Asia/Tokyo");
+        $japaneseCalender = new JapaneseCalender();
+        $ret = $japaneseCalender->get('2020-08-16');
+        assertEquals($ret['date'], '令和2年8月16日');
+    }
+
+    /**
+     * @test
+     */
+    public function 平成の年号が取得できる()
+    {
+        $japaneseCalender = new JapaneseCalender();
+        $ret = $japaneseCalender->get('1990-08-16');
+        assertEquals($ret['date'], '平成2年8月16日');
+    }
+
+    /**
+     * @test
+     */
+    public function 昭和の年号が取得できる()
+    {
+        $japaneseCalender = new JapaneseCalender();
+        $ret = $japaneseCalender->get('1950-08-16');
+        assertEquals($ret['date'], '昭和25年8月16日');
+    }
+
+    /**
+     * @test
+     */
+    public function 大正の年号が取得できる()
+    {
+        $japaneseCalender = new JapaneseCalender();
+        $ret = $japaneseCalender->get('1915-08-16');
+        assertEquals($ret['date'], '大正4年8月16日');
+    }
+
+    /**
+     * @test
+     */
+    public function 明治の年号が取得できる()
+    {
+        $japaneseCalender = new JapaneseCalender();
+        $ret = $japaneseCalender->get('1869-08-16');
+        assertEquals($ret['date'], '明治2年8月16日');
+    }
+
+    /**
+     * @test
+     */
+    public function 令和1年は令和元年で取得できる()
+    {
+        $japaneseCalender = new JapaneseCalender();
+        $ret = $japaneseCalender->get('2019-08-16');
+        assertEquals($ret['date'], '令和元年8月16日');
+    }
+
+    /**
+     * @test
+     */
+    public function 明治以前の年号は取得出来ない()
+    {
+        $this->expectException(\OutOfRangeException::class);
 
         $japaneseCalender = new JapaneseCalender();
-        // $date = new \DateTime('1979/03/12', $dateTimeZoneJapan);
-        $date = new \DateTime('2019/5/1', $dateTimeZoneJapan);
-        // $date = new \DateTime('2019/5/1', $dateTimeZoneJapan);
+        $japaneseCalender->get('1800-08-16');
+    }
 
-        var_dump($japaneseCalender->get($date));
-        // assertFalse();
+    /**
+     * @test
+     */
+    public function 日付に変換出来ない引数を投げると例外()
+    {
+        $this->expectException(InvalidFormatException::class);
+
+        $japaneseCalender = new JapaneseCalender();
+        $japaneseCalender->get('test');
     }
 }
